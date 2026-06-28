037920a62563eaf3ed4a3c66121efa6b9f30d9ea
amayonesss <148882713+amayonesss@users.noreply.github.com>
2026-06-28 21:32:50 +0700

    <td>             {% if pagination %}               {{ (pagination.page - 1) * pagination.per_page + loop.index }}             {% else %}               {{ loop.index }}             {% endif %}           </td>

--- app/master/routes.py
+++ app/master/routes.py
@@ -27,7 +27,7 @@ def master_program():
     q = request.args.get('q', '').strip()
     page = request.args.get('page', 1, type=int)
 
-    base_query = Program.query.order_by(Program.id)
+    base_query = Program.query.order_by(Program.id.desc())
 
     if q:
         base_query = base_query.filter(
